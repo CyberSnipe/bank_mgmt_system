5edcd4ea865e15803ae9b4957afe3d7e4e090ce3
CyberSnipe <n00661116@unf.edu>
2026-04-02 10:37:39 -0400

    updated environment

--- src/config.py
+++ src/config.py
@@ -3,10 +3,10 @@ import configparser
 config = configparser.ConfigParser()
 config.read("settings.cfg")
 
-DATA_DIR = config.get("ROOT", "/data_dir")
-DATA_INTERNAL = config.get("ROOT", "data/data_internal")
-DATA_EXTERNAL = config.get("ROOT", "data/data_external")
-LOGGING = config.get("ROOT", "/logging")
-DEBUG = config.getboolean("ROOT", "debug")
+DATA_DIR = config.get("path", "/data_dir")
+DATA_INTERNAL = config.get("path", "data/data_internal")
+DATA_EXTERNAL = config.get("path", "data/data_external")
+LOGGING = config.get("path", "/logging")
+DEBUG = config.getboolean("path", "debug")
 
 
